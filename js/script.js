292011c7d31f81bc7d7baf6cab3b08feaedf38e6
dknsapatin <dknsapatin@gmail.com>
2022-02-07 21:16:30 -0500

    Moved nav section up top

---
+++ js/script.js
@@ -0,0 +1,13 @@
+// const header = document.querySelector(".header");
+// const stickyNav = function (entries) {
+//   const [entry] = entries;
+//   console.log(entry);
+
+//   if (!entry.isIntersecting) nav.classList.add("sticky");
+//   else nav.classList.remove("sticky");
+// };
+// const headerObserver = new IntersectionObserver(stickyNav, {
+//   root: null,
+//   threshold: 0,
+// });
+// headerObserver.observe(header);
