59742bce45c2d0ecd4b2a668e54258f152aa2231
Denzel Sapatin <dknsapatin@gmail.com>
2022-02-23 16:16:58 -0500

    Working contact links

--- js/script.js
+++ js/script.js
@@ -1,13 +1,14 @@
-// const header = document.querySelector(".header");
-// const stickyNav = function (entries) {
-//   const [entry] = entries;
-//   console.log(entry);
+const header = document.querySelector("header");
 
-//   if (!entry.isIntersecting) nav.classList.add("sticky");
-//   else nav.classList.remove("sticky");
-// };
-// const headerObserver = new IntersectionObserver(stickyNav, {
-//   root: null,
-//   threshold: 0,
-// });
-// headerObserver.observe(header);
+const stickyNav = function (entries) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) nav.classList.add("sticky");
+  else nav.classList.remove("sticky");
+};
+const headerObserver = new IntersectionObserver(stickyNav, {
+  root: null,
+  threshold: 0,
+});
+headerObserver.observe(header);
