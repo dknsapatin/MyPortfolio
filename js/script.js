8c005a009fedd8a16136baa475e200347d49ccf7
dknsapatin <dknsapatin@gmail.com>
2022-02-23 21:58:50 -0500

    Added Sticky navigation using IntersectionObserver

--- js/script.js
+++ js/script.js
@@ -1,14 +1,26 @@
-const header = document.querySelector("header");
+//-----------Intersection Observer API---------------
+
+const header = document.querySelector(".header");
+// Grab the navigation section
+const nav = document.querySelector(".section-nav");
+const navHeight = nav.getBoundingClientRect().height;
 
 const stickyNav = function (entries) {
   const [entry] = entries;
   console.log(entry);
 
+  // If the entries is not intersecting the root: add stickty : remove sticky
   if (!entry.isIntersecting) nav.classList.add("sticky");
   else nav.classList.remove("sticky");
 };
+
+// Create an observer of intersection Observer function that takes in
+// the stickyNav, the root and the threshold of the size
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
+  rootMargin: `-${navHeight}px`,
 });
+
+// Observe the header using the header observer that was created
 headerObserver.observe(header);
